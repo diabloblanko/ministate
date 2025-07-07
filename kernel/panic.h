7e0ab9333cc1bbefa49ce6c2ab3fbaf2a8df746b
diabloblanko <softandwettoa@gmail.com>
2025-07-07 22:07:33 +0300

    Delete kernel directory

--- kernel/panic.h
+++
@@ -1,5 +0,0 @@
-// Panic.h
-// Added in MiniState v1.1
-#pragma once
-#define KPANIC(msg) kpanic(msg, __FILE__, __LINE__)
-void kpanic(const char* message, const char* file, int line);
--- kernel/screen.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCREEN_H
-#define SCREEN_H
-void clears();
-void putstr(const char* str);
-void putchr(char c);
-void update_cursor();
-void setcolor(uint8_t color);
-uint8_t get_text_color;
-#endif
