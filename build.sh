af423de7026567b24a60c20acc7245b814546728
diabloblanko <softandwettoa@gmail.com>
2025-07-07 22:12:09 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-# Compile ASM files
-nasm -f elf32 entry.asm -o entry.o
-nasm -f elf32 ports.asm -o ports.o
-
-# Compile C files
-gcc -m32 -c kernel.c -o ministate-kernel.o -ffreestanding -nostdlib
-gcc -m32 -c screen.c -o screen.o -ffreestanding -nostdlib
-
-# Linking tiime:P
-ld -m elf_i386 -T link.ld -o ministate-1 entry.o ports.o ministate-kernel.o screen.o
+  1 #!/bin/bash
+  2 gcc -m32 -c screen.c  -o screen.o -ffreestanding -nostdlib
+  3 gcc -m32 -c kernel.c  -o kernel.o -ffreestanding -nostdlib
+  4 #gcc -m32 kpanic.c  -o kpanic.o    -ffreestanding -nostdlib
+  5 nasm -f elf32 entry.asm -o entry.o
+  6 #nasm -f elf32 kernel.asm -o asmkernel.o
+  7 nasm -f elf32 ports.asm -o ports.o
+  8 ld -m elf_i386 -T link.ld -o ministate-2-test entry.o ports.o kernel.o screen.o
+  9 echo "Building finished"
