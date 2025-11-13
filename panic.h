6ba009ad31a8f33b65f936140e3c55adf92a8f6e
diabloblanko <softandwettoa@gmail.com>
2025-11-14 01:31:11 +0300

    fast fix

--- panic.h
+++
@@ -1,7 +0,0 @@
-// Panic.h
-// Copyright(c) 2025 DiabloBlanco
-// MiniState project, V3R
-// Added in MiniState v1.1
-#pragma once
-#define KPANIC(msg) kpanic(msg, __FILE__, __LINE__)
-void kpanic(const char* message, const char* file, int line);
